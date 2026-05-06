63b2679f13692583d68d32561f4f6868114d22e1
winnerx0 <eragbonikpeya@gmail.com>
2026-05-06 14:07:57 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is required")
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Deployed app on port " + port)
+	})
+
+	log.Printf("listening on port %s", port)
+	log.Fatal(app.Listen(":" + port))
+}
